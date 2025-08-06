791a0b6b690f998a4b7896c1ded77d4998f2be75
Can Guvercin <mustafacanguvercin@gmail.com>
2025-08-07 00:41:39 +0300

    before challenge

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,12 +1,46 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import {useQuery} from '@tanstack/react-query';
+import { fetchEvents } from '../util/http';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const {data, isPending, isError, error} = useQuery({
+    queryKey: ['events', {search: searchTerm}],
+    queryFn: () => fetchEvents(searchTerm),
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
   }
 
+  let content = <p>Please enter a search term and to find event</p>;
+  if (isPending) {
+    content = <p>Loading...</p>;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock title="An error" message={error.info?.message || 'Failed to fetch events.'}/>
+
+    );
+  }
+  
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  
   return (
     <section className="content-section" id="all-events-section">
       <header>
